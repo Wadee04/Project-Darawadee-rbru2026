5061fdeee8d107f10120145c5e87af129f99d25f
Wadee04 <6614631011@rbru.ac.th>
2026-09-18 11:22:40 +0700

    auto: update (2026-09-18 11:22:40)

--- lib/view/User/Signup/email_otp_page.dart
+++ lib/view/User/Signup/email_otp_page.dart
@@ -14,12 +14,10 @@ class EmailOtpPage extends StatefulWidget {
   const EmailOtpPage({
     super.key,
     required this.email,
-    required this.password,
     required this.fullName,
   });
 
   final String email;
-  final String password;
   final String fullName;
 
   @override
