c153ba7f08ca64532a5d41771424aa6bc7a893f5
Wadee04 <6614631011@rbru.ac.th>
2026-09-18 10:05:23 +0700

    auto: update (2026-09-18 10:05:23)

--- lib/view/User/Profile/change_password.dart
+++ lib/view/User/Profile/change_password.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../components/shared_widgets.dart';
+import '../../../services/service_locator.dart';
 import '../../../theme/app_colors.dart';
 import '../../../theme/responsive.dart';
 
