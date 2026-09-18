82eb3337131aef391ae99bb336642e72660cc295
Wadee04 <6614631011@rbru.ac.th>
2026-09-18 09:41:30 +0700

    auto: update (2026-09-18 09:41:30)

--- lib/view/User/Signup/signup.dart
+++ lib/view/User/Signup/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' show AuthException;
 
 import '../../../components/shared_widgets.dart';
 import '../../../services/service_locator.dart';
